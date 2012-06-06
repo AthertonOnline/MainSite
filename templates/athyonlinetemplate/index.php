48b3e3497a7c705b5ffda1c49c0636ec8dc843c6
Alex Hartley <alex.hartley@gmail.com>
2012-06-06 16:05:09 +0100

    Revert "Revert "1A""
    This reverts commit 1d6818c0dde0933f4ee65e7da4fda74ead09e3a8.

--- templates/athyonlinetemplate/index.php
+++ templates/athyonlinetemplate/index.php
@@ -23,7 +23,7 @@
 				</div>
 				<div class="clear"></div>
 			</div>
-			<h1>I am standing in the Atherleigh Ward and I am urging everyone to come out and vote.</h1>
+			<h1>sdjkkjh kjsd fsalkjf skhf skldfh slkfh sadlkfjhsdflksadh flkjsadh fkljsahf aslf safk kfjh lkfjsd f</h1>
 			<div class="search">
 			<jdoc:include type="modules" name="search" />
 			</div>
